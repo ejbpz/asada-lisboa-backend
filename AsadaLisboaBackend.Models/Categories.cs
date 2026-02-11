ee5e10e45c47c99ac76292959737ed46b11e673c
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-02-10 22:03:54 -0600

    feat: New database models (News and Categories).

---
+++ AsadaLisboaBackend.Models/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsadaLisboaBackend.Models
+{
+    public class Categories
+    {
+        [Key]
+        public string Id { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(5)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
