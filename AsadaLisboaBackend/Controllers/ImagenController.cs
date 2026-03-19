d846e477f363ebc9af2c5e618c8dad17b5c2fd8f
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-19 11:51:40 -0600

    Add Imagecontroller

---
+++ AsadaLisboaBackend/Controllers/ImagenController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AsadaLisboaBackend.ServiceContracts.Image;
+using AsadaLisboaBackend.Models.DTOs.Image;
+
+namespace AsadaLisboaBackend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagenController : ControllerBase
+    {
+        private readonly IImageService _imageService;
+
+        public ImagenController(IImageService imageService)
+        {
+
+            _imageService = imageService;
+        }
+
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> CreateImage([FromForm] ImageRequestDTO imageRequestDTO)
+        {
+            var result = await _imageService.CreateImage(imageRequestDTO);
+            return Ok(result);
+        }
+
+
+    }
+}
