041f9fc560019e7607b7d7dcc4375500f18ad025
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-20 21:05:45 -0600

    Modified ImageController to add edit and delete image

--- AsadaLisboaBackend/Controllers/ImagenController.cs
+++ AsadaLisboaBackend/Controllers/ImagenController.cs
@@ -18,13 +18,42 @@ namespace AsadaLisboaBackend.Controllers
         }
 
         [HttpPost]
-        [Route("create")]
+        [Route("creacion")]
         public async Task<IActionResult> CreateImage([FromForm] ImageRequestDTO imageRequestDTO)
         {
             var result = await _imageService.CreateImage(imageRequestDTO);
             return Ok(result);
         }
 
+        [HttpPut]
+        [Route("edicion")]
+        public async Task<IActionResult> Edit([FromForm] ImageUpdateRequestDTO ImageUpdateRequestDTO)
+        {
+            try
+            {
+                var result = await _imageService.UpdateImage(ImageUpdateRequestDTO);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpDelete]
+        [Route("delete/{id}/{slug}")]
+        public async Task<IActionResult> Delete(Guid id, string slug)
+        {
+            try
+            {
+                var result = await _imageService.DeleteImage(id, slug);
+                return Ok(new { success = result, message = "Imagen eliminada correctamente" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
 
     }
 }
