328a169fd04056dd75e2921a04ad5bfaddd9bd8d
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-23 16:40:06 -0600

    Change name of file

---
+++ AsadaLisboaBackend.Utils/DocumentAttribute/AllowedExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace AsadaLisboaBackend.Utils.DocumentAtribute
+{
+    public class AllowedExtensions(string[] extensions) : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLower();
+                if (!extensions.Contains(extension))
+                    return new ValidationResult("Formato de documento no permitido.");
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
