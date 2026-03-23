6032f2f602ae0d03a4453049d6adbd69770c8c22
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-23 16:22:33 -0600

    Add DocumentAtribute

---
+++ AsadaLisboaBackend.Utils/DocumentAtribute/MaxFileSizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace AsadaLisboaBackend.Utils.DocumentAtribute
+{
+    public class MaxFileSizeAttribute(int maxFileSizeMb) : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is IFormFile file)
+            {
+                // Convertir MB a Bytes
+                var maxBytes = maxFileSizeMb * 1024 * 1024;
+
+                if (file.Length > maxBytes)
+                {
+                    return new ValidationResult($"El archivo no puede pesar más de {maxFileSizeMb} MB.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
