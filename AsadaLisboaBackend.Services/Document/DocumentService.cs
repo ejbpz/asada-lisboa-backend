9323584fef8037d315620da0a42b8b4ce2f9e05d
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-24 16:48:34 -0600

    Modified DocumentService to add UpdateDocument

--- AsadaLisboaBackend.Services/Document/DocumentService.cs
+++ AsadaLisboaBackend.Services/Document/DocumentService.cs
@@ -77,5 +77,70 @@ namespace AsadaLisboaBackend.Services.Document
                 throw new CreateObjectException("Error al crear la imagen.");
             }
         }
+
+        public async Task<DocumentResponseDTO> UpdateDocument(Guid id, DocumentUpdateRequestDTO documentUpdateRequestDTO, FileStorageOptions fileStorageOptions)
+        {
+
+
+            var document = await _applicationDbContext.Document
+                .Include(i => i.Categories)
+                .FirstOrDefaultAsync(i => i.Id == id);
+
+            if (document is null)
+                throw new NotFoundException("Documento no encontrada.");
+
+            document.Title = documentUpdateRequestDTO.Title;
+            document.StatusId = documentUpdateRequestDTO.StatusId;
+            document.Description = documentUpdateRequestDTO.Description;
+
+            document.Slug = GenerateSlug.New(documentUpdateRequestDTO.Title, document.Id);
+
+            var categories = await _applicationDbContext.Categories
+                .Where(c => documentUpdateRequestDTO.CategoryIds.Contains(c.Id))
+                .ToListAsync();
+
+            document.Categories.Clear();
+            document.Categories = categories;
+
+            if (documentUpdateRequestDTO.File is null || documentUpdateRequestDTO.File.Length <= 0)
+                throw new NotFoundException("Imagen no encontrada.");
+
+            var extension = Path.GetExtension(documentUpdateRequestDTO.File.FileName).ToLowerInvariant();
+            var newFileName = $"{document.Id}{extension}";
+            var newFilePath = Path.Combine(fileStorageOptions.BasePath, newFileName);
+
+            try
+            {
+                using (var stream = new FileStream(newFilePath, FileMode.Create))
+                {
+                    await documentUpdateRequestDTO.File.CopyToAsync(stream);
+                }
+
+                if (!string.IsNullOrEmpty(document.FilePath) && File.Exists(document.FilePath) && document.FilePath != newFilePath)
+                    File.Delete(document.FilePath);
+
+                document.FileName = newFileName;
+                document.FilePath = newFilePath;
+                document.FileSize = documentUpdateRequestDTO.File.Length;
+            }
+            catch
+            {
+                if (File.Exists(newFilePath))
+                    File.Delete(newFilePath);
+
+                throw new UpdateObjectException("Error al actualizar el document.");
+            }
+
+            var affectedRows = await _applicationDbContext.SaveChangesAsync();
+
+            if (affectedRows < 1)
+                throw new NotFoundException("Document no encontrada.");
+
+            return document.ToDocumentResponseDTO();
+
+        }
+
+
+
     }
 }
