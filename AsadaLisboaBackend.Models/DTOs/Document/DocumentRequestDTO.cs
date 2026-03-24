9bc88fd8a69814135fad3879b19d6744007879c0
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-23 20:44:40 -0600

    Modified DocumentRequestDTO

--- AsadaLisboaBackend.Models/DTOs/Document/DocumentRequestDTO.cs
+++ AsadaLisboaBackend.Models/DTOs/Document/DocumentRequestDTO.cs
@@ -18,5 +18,11 @@ namespace AsadaLisboaBackend.Models.DTOs.Document
         [AllowedExtensions(new string[] { ".pdf", ".docx", ".xlsx" })]
         [MaxFileSize(5)] // Límite de 5 MB
         public IFormFile File { get; set; } = null!;
+
+        public Guid StatusId { get; set; }
+
+        public Guid DocumentTypeId { get; set; }
+
+        public List<Guid> CategoryIds { get; set; } = new();
     }
 }
