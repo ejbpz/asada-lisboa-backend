a9056e288baf7471bb18ffe84a0a4616c450d5fc
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-24 15:25:20 -0600

    Modified DocumentService

--- AsadaLisboaBackend.ServiceContracts/Document/IDocumentService.cs
+++ AsadaLisboaBackend.ServiceContracts/Document/IDocumentService.cs
@@ -1,9 +1,10 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Document;
+using AsadaLisboaBackend.Utils.OptionsPattern;
 
 namespace AsadaLisboaBackend.ServiceContracts.Document
 {
    public interface IDocumentService
     {
-        public Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO);
+        public Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO, FileStorageOptions fileStorageOptions);
     }
 }
