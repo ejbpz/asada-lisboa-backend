44754ca331816f352d107411f7b3af6705520087
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-24 16:43:13 -0600

    Modified IDocumentService to add UpdateDocument

--- AsadaLisboaBackend.ServiceContracts/Document/IDocumentService.cs
+++ AsadaLisboaBackend.ServiceContracts/Document/IDocumentService.cs
@@ -6,5 +6,7 @@ namespace AsadaLisboaBackend.ServiceContracts.Document
    public interface IDocumentService
     {
         public Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO, FileStorageOptions fileStorageOptions);
+
+        public Task<DocumentResponseDTO> UpdateDocument(Guid id, DocumentUpdateRequestDTO documentUpdateRequestDTO, FileStorageOptions fileStorageOptions);
     }
 }
