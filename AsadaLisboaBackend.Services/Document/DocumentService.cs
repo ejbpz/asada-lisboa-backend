5f02c9c0a1a059bd4e573fad7b1beb4139d0ffb7
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-24 15:29:11 -0600

    Modified DocumentService

--- AsadaLisboaBackend.Services/Document/DocumentService.cs
+++ AsadaLisboaBackend.Services/Document/DocumentService.cs
@@ -1,6 +1,9 @@
-﻿using AsadaLisboaBackend.Models.DatabaseContext;
+﻿using Microsoft.EntityFrameworkCore;
+using AsadaLisboaBackend.Models.DatabaseContext;
 using AsadaLisboaBackend.Models.DTOs.Document;
-using AsadaLisboaBackend.Models.DTOs.Image;
+using AsadaLisboaBackend.Services.Exceptions;
+using AsadaLisboaBackend.Utils.OptionsPattern;
+using AsadaLisboaBackend.Utils.SlugGeneration;
 using AsadaLisboaBackend.ServiceContracts.Document;
 
 
@@ -15,50 +18,64 @@ namespace AsadaLisboaBackend.Services.Document
             _applicationDbContext = applicationDbContext;
         }
 
-        public async Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO)
+        public async Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO, FileStorageOptions fileStorageOptions)
         {
-             // Guardar archivo físico
-           
-            var fileName = $"{Guid.NewGuid()}_{documentRequestDTO.File.FileName}";
-            var filePath = Path.Combine(AppContext.BaseDirectory, "uploads", fileName);
 
-            using (var stream = new FileStream(filePath, FileMode.Create))
+            if (documentRequestDTO.File is null || documentRequestDTO.File.Length == 0)
+                throw new ArgumentException("Archivo inválido.");
+
+            var documentId = Guid.NewGuid();
+            var extension = Path.GetExtension(documentRequestDTO.File.FileName).ToLowerInvariant();
+
+            var fileName = $"{documentId}{extension}";
+            var filePath = Path.Combine(fileStorageOptions.BasePath, fileName);
+
+            if (!Directory.Exists(fileStorageOptions.BasePath))
+                Directory.CreateDirectory(fileStorageOptions.BasePath);
+
+            try
             {
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await documentRequestDTO.File.CopyToAsync(stream);
+                }
 
-                await documentRequestDTO.File.CopyToAsync(stream);
+                
+                var slug = GenerateSlug.New(documentRequestDTO.Title, documentId);
+                var categories = await _applicationDbContext.Categories
+                    .Where(c => documentRequestDTO.CategoryIds.Contains(c.Id))
+                    .ToListAsync();
+
+                var newDocument = new Models.Document()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = documentRequestDTO.Title,
+                    Description = documentRequestDTO.Description,
+                    Slug = documentRequestDTO.Title.ToLower().Replace(" ", "-"),
+                    PublicationDate = DateTime.UtcNow,
+                    FileSize = documentRequestDTO.File.Length,
+                    StatusId = documentRequestDTO.StatusId,
+                    DocumentTypeId = documentRequestDTO.DocumentTypeId,
+                    Categories = _applicationDbContext.Categories
+                    .Where(c => documentRequestDTO.CategoryIds.Contains(c.Id))
+                    .ToList()
+                };
+
+                _applicationDbContext.Documents.Add(newDocument);
+                var affectedRows = await _applicationDbContext.SaveChangesAsync();
+
+                if (affectedRows < 1)
+                    throw new NotFoundException("Imagen no encontrada.");
+
+                return newDocument.ToDocumentResponseDTO();
             }
-
-
-
-            var newDocument = new Models.Document()
+            catch
             {
-                Id = Guid.NewGuid(),
-                Title = documentRequestDTO.Title,
-                Description = documentRequestDTO.Description,
-                Slug = documentRequestDTO.Title.ToLower().Replace(" ", "-"),
-                PublicationDate = DateTime.UtcNow,
-                FileSize = documentRequestDTO.File.Length,
-                StatusId = documentRequestDTO.StatusId,
-                DocumentTypeId = documentRequestDTO.DocumentTypeId,
-                Categories = _applicationDbContext.Categories
-                .Where(c => documentRequestDTO.CategoryIds.Contains(c.Id))
-                .ToList()
-            };
+                if (File.Exists(filePath))
+                    File.Delete(filePath);
 
-            _applicationDbContext.Documents.Add(newDocument);
-            await _applicationDbContext.SaveChangesAsync();
-
-            return new  DocumentResponseDTO{
-
-                Id = newDocument.Id,
-                Slug = newDocument.Slug,
-                Title = newDocument.Title,
-                Description = newDocument.Description,
-                PublicationDate = newDocument.PublicationDate,
-                FileSize = newDocument.FileSize,
-                StatusName = newDocument.Status?.Name ?? string.Empty,
-                Categories = newDocument.Categories.Select(c => c.Name).ToList()
-            };
+                throw new CreateObjectException("Error al crear la imagen.");
+            }
         }
     }
 }
