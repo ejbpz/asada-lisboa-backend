d4ad64070dea8a69b797813f4f2cd5a4a367053b
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-25 13:22:15 -0600

    Add DocumentGetterService

---
+++ AsadaLisboaBackend.Services/Document/DocumentGetterService.cs
@@ -0,0 +1,34 @@
+﻿using AsadaLisboaBackend.ServiceContracts.Document;
+using AsadaLisboaBackend.Models.DTOs.Shared;
+using AsadaLisboaBackend.Models.DTOs.Document;
+using AsadaLisboaBackend.RepositoryContracts.Document;
+
+namespace AsadaLisboaBackend.Services.Document
+{
+    public class DocumentGetterService : IDocumentGetterService
+    {
+        private readonly IDocumentGetterRepository _documentGetterRepository;
+
+        public DocumentGetterService(IDocumentGetterRepository documentGetterRepository)
+        {
+            _documentGetterRepository = documentGetterRepository;
+        }
+
+
+        public async Task<PageResponseDTO<DocumentResponseDTO>> GetDocument(SearchSortRequestDTO searchSortRequestDTO)
+        {
+            searchSortRequestDTO.Offset = (Math.Max(searchSortRequestDTO.Page, 1) - 1) * searchSortRequestDTO.Take;
+
+            return await _documentGetterRepository.GetDocument(searchSortRequestDTO);
+
+        }
+
+
+
+        public async Task<DocumentResponseDTO> GetDocument(Guid id) 
+        {
+            return await _documentGetterRepository.GetDocument(id);
+
+        }
+    }
+}
