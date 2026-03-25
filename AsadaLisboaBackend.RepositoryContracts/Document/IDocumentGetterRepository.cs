715a1111a4b5b38bfe885c4acf9ad95d24a3a0d6
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-25 13:16:51 -0600

    Add IDocumentGetterRepository

---
+++ AsadaLisboaBackend.RepositoryContracts/Document/IDocumentGetterRepository.cs
@@ -0,0 +1,12 @@
+﻿using AsadaLisboaBackend.Models.DTOs.Document;
+using AsadaLisboaBackend.Models.DTOs.Shared;
+
+namespace AsadaLisboaBackend.RepositoryContracts.Document
+{
+    public interface IDocumentGetterRepository
+    {
+        public Task<PageResponseDTO<DocumentResponseDTO>> GetDocument(SearchSortRequestDTO searchSortRequestDTO);
+
+        public Task<DocumentResponseDTO> GetDocument(Guid id);
+    }
+}
