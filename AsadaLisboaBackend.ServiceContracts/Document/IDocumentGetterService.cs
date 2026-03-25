208e8ae4449316c8f31baf70658091c4ea2394b9
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-25 13:12:03 -0600

    Add IDocumentGetterService

---
+++ AsadaLisboaBackend.ServiceContracts/Document/IDocumentGetterService.cs
@@ -0,0 +1,11 @@
+﻿using AsadaLisboaBackend.Models.DTOs.Shared;
+using AsadaLisboaBackend.Models.DTOs.Document;
+
+namespace AsadaLisboaBackend.ServiceContracts.Document
+{
+    public interface IDocumentGetterService
+    {
+        public Task<PageResponseDTO<DocumentResponseDTO>> GetDocument(SearchSortRequestDTO searchSortRequestDTO);
+        public Task<DocumentResponseDTO> GetDocument(Guid id);
+    }
+}
