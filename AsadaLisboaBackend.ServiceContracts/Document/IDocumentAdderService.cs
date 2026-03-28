68cd2143ea4e7112e18f92e105b78cf8599d7e95
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-03-28 15:37:16 -0600

    Add IDocumentAdderService to ServiceContracts

---
+++ AsadaLisboaBackend.ServiceContracts/Document/IDocumentAdderService.cs
@@ -0,0 +1,9 @@
+﻿using AsadaLisboaBackend.Models.DTOs.Document;
+
+namespace AsadaLisboaBackend.ServiceContracts.Document
+{
+    public interface IDocumentAdderService
+    {
+        public Task<DocumentResponseDTO> CreateDocument(DocumentRequestDTO documentRequestDTO)
+    }
+}
