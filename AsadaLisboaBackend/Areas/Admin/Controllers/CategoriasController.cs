140ff0e4234e94e8eadc106d8e593d403c1c3ac1
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-03-31 15:19:24 -0600

    Controller categorias using CategoriesDeleterService.

---
+++ AsadaLisboaBackend/Areas/Admin/Controllers/CategoriasController.cs
@@ -0,0 +1,27 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using AsadaLisboaBackend.ServiceContracts.Categories;
+
+namespace AsadaLisboaBackend.Areas.Admin.Controllers
+{
+    [ApiController]
+    [Area("Admin")]
+    [ApiVersion("1.0")]
+    [Route("api/[area]/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ICategoriesDeleterService _categoriesDeleterService;
+
+        public CategoriasController(ICategoriesDeleterService categoriesDeleterService)
+        {
+            _categoriesDeleterService = categoriesDeleterService;
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+            await _categoriesDeleterService.DeleteCategory(id);
+            return NoContent();
+        }
+    }
+}
