efc007b400d53b1868a5c8b3dfcddc29911e0a31
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-02 19:26:50 -0600

    Adding Contactos controller to public site.

---
+++ AsadaLisboaBackend/Areas/Cliente/Controllers/ContactosController.cs
@@ -0,0 +1,39 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using AsadaLisboaBackend.Models.DTOs.Shared;
+using AsadaLisboaBackend.Models.DTOs.Contact;
+using AsadaLisboaBackend.ServiceContracts.Contacts;
+
+namespace AsadaLisboaBackend.Areas.Cliente.Controllers
+{
+    [ApiController]
+    [AllowAnonymous]
+    [Area("Cliente")]
+    [ApiVersion("1.0")]
+    [Route("api/[area]/[controller]")]
+    public class ContactosController : ControllerBase
+    {
+        private readonly IContactsGetterService _contactsGetterService;
+
+        public ContactosController(IContactsGetterService contactsGetterService)
+        {
+            _contactsGetterService = contactsGetterService;
+        }
+
+        [HttpGet("")]
+        public async Task<ActionResult<PageResponseDTO<ContactResponseDTO>>> GetContacts()
+        {
+            return Ok(await _contactsGetterService.GetContacts(new SearchSortRequestDTO()
+            {
+                Page = 1,
+                Take = 100,
+                Offset = 0,
+                Search = null,
+                FilterBy = null,
+                SortBy = "order",
+                SortDirection = "asc",
+            }));
+        }
+    }
+}
