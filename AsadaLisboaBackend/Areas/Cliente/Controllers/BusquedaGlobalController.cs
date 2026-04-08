30ea4e4972f01b4e85c119753b6dd1004320f5c8
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-08 12:07:26 -0600

    Add BusquedaGlobalController

---
+++ AsadaLisboaBackend/Areas/Cliente/Controllers/BusquedaGlobalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Asp.Versioning;
+using AsadaLisboaBackend.ServiceContracts.SearchGlobal;
+
+namespace AsadaLisboaBackend.Areas.Cliente.Controllers
+{
+    [Area("Cliente")]
+    [ApiVersion("1.0")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class BusquedaGlobalController : ControllerBase
+    {
+        private readonly ISearchGlobalService _searchGlobal;
+
+        public BusquedaGlobalController(ISearchGlobalService searchGlobal)
+        {
+            _searchGlobal = searchGlobal;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Search(string q)
+        {
+            var result = await _searchGlobal.SearchAsync(q);
+            return Ok(result);
+        }
+
+    }
+}
