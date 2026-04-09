0615d36d903deba361286feec7633fd6a6f68649
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 17:18:00 -0600

    Add Caching to ConfigurationsUpdaterService

--- AsadaLisboaBackend.Services/Configurations/ConfigurationsUpdaterService.cs
+++ AsadaLisboaBackend.Services/Configurations/ConfigurationsUpdaterService.cs
@@ -1,6 +1,9 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.Configuration;
-using AsadaLisboaBackend.ServiceContracts.Configurations;
 using AsadaLisboaBackend.RepositoryContracts.Configurations;
+using AsadaLisboaBackend.ServiceContracts.Configurations;
+using AsadaLisboaBackend.ServiceContracts.MemoryCaches;
+using AsadaLisboaBackend.Services.MemoryCaches;
+using AsadaLisboaBackend.Utils;
 using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Configurations
@@ -9,10 +12,12 @@ namespace AsadaLisboaBackend.Services.Configurations
     {
         private readonly IConfigurationsUpdaterRepository _configurationsUpdaterRepository;
         private readonly ILogger<ConfigurationsUpdaterService> _logger;
+        private readonly IMemoryCachesService _memoryCachesService;
 
-        public ConfigurationsUpdaterService(IConfigurationsUpdaterRepository configurationsUpdaterRepository, ILogger<ConfigurationsUpdaterService> logger)
+        public ConfigurationsUpdaterService(IConfigurationsUpdaterRepository configurationsUpdaterRepository, ILogger<ConfigurationsUpdaterService> logger, IMemoryCachesService memoryCachesService)
         {
             _configurationsUpdaterRepository = configurationsUpdaterRepository;            _logger = logger;
+            _memoryCachesService = memoryCachesService;
 
         }
 
@@ -25,6 +30,9 @@ namespace AsadaLisboaBackend.Services.Configurations
             }
 
             _logger.LogInformation("Actualización exitosa de configuración con Id: {Id}", id);
+
+            _memoryCachesService.RemoveById(Constants.CACHE_CONFIGURATIONS, id);
+            _memoryCachesService.ChangeVersion(Constants.CACHE_CONFIGURATIONS);
 
             return  result.ToConfigurationResponseDTO();
         }
