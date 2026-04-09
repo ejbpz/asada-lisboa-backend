53ea741fd172d5941473893e4feee1cd22c616f8
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 11:05:58 -0600

    Add Logging to ConfigurationsDeleterService

--- AsadaLisboaBackend.Services/Configurations/ConfigurationsDeleterService.cs
+++ AsadaLisboaBackend.Services/Configurations/ConfigurationsDeleterService.cs
@@ -1,20 +1,33 @@
 ﻿using AsadaLisboaBackend.ServiceContracts.Configurations;
 using AsadaLisboaBackend.RepositoryContracts.Configurations;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Configurations
 {
     public class ConfigurationsDeleterService : IConfigurationsDeleterService
     {
         private readonly IConfigurationsDeleterRepository _configurationsDeleterRepository;
+        private readonly ILogger<ConfigurationsDeleterService> _logger;
 
-        public ConfigurationsDeleterService(IConfigurationsDeleterRepository configurationsDeleterRepository)
+        public ConfigurationsDeleterService(IConfigurationsDeleterRepository configurationsDeleterRepository, ILogger<ConfigurationsDeleterService> logger)
         {
             _configurationsDeleterRepository = configurationsDeleterRepository;
+            _logger = logger;
         }
 
         public async Task UpdateConfiguration(Guid id)
         {
-            await _configurationsDeleterRepository.DeleteConfiguration(id);
+            try 
+            {
+              await _configurationsDeleterRepository.DeleteConfiguration(id);
+                _logger.LogInformation("Configuración con Id: {Id} eliminado exitosamente.", id);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error eliminar  configuración con Id: {Id}", id);
+                throw;
+            }
+
         }
     }
 }
