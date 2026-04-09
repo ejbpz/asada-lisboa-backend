976c6f33821c83fc3a29f1caf1b04468485b541b
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 11:06:53 -0600

    Add Logging to ConfigurationsGetterService

--- AsadaLisboaBackend.Services/Configurations/ConfigurationsGetterService.cs
+++ AsadaLisboaBackend.Services/Configurations/ConfigurationsGetterService.cs
@@ -2,23 +2,45 @@
 using AsadaLisboaBackend.Models.DTOs.Configuration;
 using AsadaLisboaBackend.ServiceContracts.Configurations;
 using AsadaLisboaBackend.RepositoryContracts.Configurations;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.Configurations
 {
     public class ConfigurationsGetterService : IConfigurationsGetterService
     {
         private readonly IConfigurationsGetterRepository _configurationsGetterRepository;
+        private readonly ILogger<ConfigurationsGetterService> _logger;
 
-        public ConfigurationsGetterService(IConfigurationsGetterRepository configurationsGetterRepository)
+        public ConfigurationsGetterService(IConfigurationsGetterRepository configurationsGetterRepository, ILogger<ConfigurationsGetterService> logger)
         {
             _configurationsGetterRepository = configurationsGetterRepository;
+            _logger = logger;
         }
 
         public async Task<PageResponseDTO<ConfigurationResponseDTO>> GetConfigurations(SearchSortRequestDTO searchSortRequestDTO)
         {
-            searchSortRequestDTO.Offset = (Math.Max(searchSortRequestDTO.Page, 1) - 1) * searchSortRequestDTO.Take;
+            try
+            {
+                searchSortRequestDTO.Offset = (Math.Max(searchSortRequestDTO.Page, 1) - 1) * searchSortRequestDTO.Take;
 
-            return await _configurationsGetterRepository.GetConfigurations(searchSortRequestDTO);
-        }
+                var result  = await _configurationsGetterRepository.GetConfigurations(searchSortRequestDTO);
+
+                _logger.LogInformation(
+                    "Obtención exitosa de configuración. Página: {Page}, Tamaño: {Take}",
+                    searchSortRequestDTO.Page,
+                    searchSortRequestDTO.Take
+                );
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,
+                    "Error al obtener configuración. Página: {Page}, Tamaño: {Take}",
+                    searchSortRequestDTO.Page,
+                    searchSortRequestDTO.Take
+                );
+                throw;
+            }
     }
 }
