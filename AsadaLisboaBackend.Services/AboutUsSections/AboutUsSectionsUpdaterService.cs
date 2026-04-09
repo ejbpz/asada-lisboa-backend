4f0b45f17574bc5f5c81ab4910baad77ac213f4e
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 09:53:19 -0600

    Add Logging to AboutUsSectionsUpdaterService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsUpdaterService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsUpdaterService.cs
@@ -1,22 +1,33 @@
 ﻿using AsadaLisboaBackend.Models.DTOs.AboutUs;
 using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
     public class AboutUsSectionsUpdaterService : IAboutUsSectionsUpdaterService
     {
         private readonly IAboutUsSectionsUpdaterRepository _aboutUsSectionsUpdaterRepository;
+        private readonly ILogger<AboutUsSectionsUpdaterService> _logger;
 
-        public AboutUsSectionsUpdaterService(IAboutUsSectionsUpdaterRepository aboutUsSectionsUpdaterRepository)
+        public AboutUsSectionsUpdaterService(IAboutUsSectionsUpdaterRepository aboutUsSectionsUpdaterRepository, ILogger<AboutUsSectionsUpdaterService> logger)
         {
             _aboutUsSectionsUpdaterRepository = aboutUsSectionsUpdaterRepository;
+            _logger = logger;
         }
 
         public async Task<AboutUsResponseDTO> UpdateAboutUsSection(Guid id, AboutUsRequestDTO aboutUsRequestDTO)
         {
-            return (await _aboutUsSectionsUpdaterRepository.UpdateAboutUsSection(id, aboutUsRequestDTO))
-                .ToAboutUsResponseDTO();
+            var result = await _aboutUsSectionsUpdaterRepository.UpdateAboutUsSection(id, aboutUsRequestDTO);
+
+            if (result = null)
+            {
+                _logger.LogWarning("No se encontró AboutUsSection para actualizar con Id: {Id}", id);
+            }
+
+            _logger.LogInformation("Actualización exitosa de AboutUsSection con Id: {Id}", id);
+
+            return result.ToAboutUsResponseDTO();
         }
     }
 }
