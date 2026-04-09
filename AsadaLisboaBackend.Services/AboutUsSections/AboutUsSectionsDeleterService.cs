839f7b5c9f5650884353f7f4483e454c56205bcb
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 09:51:08 -0600

    Add Logging to AboutUsSectionsDeleterService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsDeleterService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsDeleterService.cs
@@ -1,20 +1,33 @@
 ﻿using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
+using Microsoft.Extensions.Logging;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
     public class AboutUsSectionsDeleterService : IAboutUsSectionsDeleterService
     {
         private readonly IAboutUsSectionsDeleterRepository _aboutUsSectionsDeleterRepository;
+        private readonly ILogger<AboutUsSectionsDeleterService> _logger;
 
-        public AboutUsSectionsDeleterService(IAboutUsSectionsDeleterRepository aboutUsSectionsDeleterRepository)
+        public AboutUsSectionsDeleterService(IAboutUsSectionsDeleterRepository aboutUsSectionsDeleterRepository, ILogger<AboutUsSectionsDeleterService> logger)
         {
             _aboutUsSectionsDeleterRepository = aboutUsSectionsDeleterRepository;
+            _logger = logger;
         }
 
         public async Task DeleteAboutUsSection(Guid id)
         {
-            await _aboutUsSectionsDeleterRepository.DeleteAboutUsSection(id);
+            try
+            {
+                await _aboutUsSectionsDeleterRepository.DeleteAboutUsSection(id);
+
+                _logger.LogInformation("Sobre nosotros con Id: {Id} eliminado exitosamente.", id);
+            }
+            catch (Exception ex)
+    {
+                _logger.LogError(ex, "Error eliminar  sobre nosotros con Id: {Id}", id);
+                throw;
+            }
         }
     }
 }
