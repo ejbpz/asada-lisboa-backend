3e6ea12672c86d11ba952d9383ac1af543b3c1b4
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 15:14:56 -0600

    Add Caching to AboutUsSectionsAdderService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsAdderService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsAdderService.cs
@@ -3,6 +3,8 @@ using AsadaLisboaBackend.Models.DTOs.AboutUs;
 using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
 using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 using Microsoft.Extensions.Logging;
+using AsadaLisboaBackend.ServiceContracts.MemoryCaches;
+using AsadaLisboaBackend.Utils;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
@@ -10,11 +12,13 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
     {
         private readonly IAboutUsSectionsAdderRepository _aboutUsSectionsAdderRepository;
         private readonly ILogger<AboutUsSectionsAdderService> _logger;
+        private readonly IMemoryCachesService _memoryCachesService;
 
-        public AboutUsSectionsAdderService(IAboutUsSectionsAdderRepository aboutUsSectionsAdderRepository, ILogger<AboutUsSectionsAdderService> logger)
+        public AboutUsSectionsAdderService(IAboutUsSectionsAdderRepository aboutUsSectionsAdderRepository, ILogger<AboutUsSectionsAdderService> logger, IMemoryCachesService memoryCachesService)
         {
             _aboutUsSectionsAdderRepository = aboutUsSectionsAdderRepository;
             _logger = logger;
+            _memoryCachesService = memoryCachesService;
         }
 
         public async Task<AboutUsResponseDTO> CreateAboutUsSection(AboutUsRequestDTO aboutUsRequestDTO)
@@ -30,6 +34,8 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
             var aboutUsCreate = await _aboutUsSectionsAdderRepository.CreateAboutUsSection(aboutUsSection);
 
             _logger.LogInformation("Sobre nosotros creada");
+
+            _memoryCachesService.ChangeVersion(Constants.CACHE_ABOUT_US);
 
             return aboutUsCreate.ToAboutUsResponseDTO();
 
