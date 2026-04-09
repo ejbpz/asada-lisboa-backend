853b93f028a50179ff06cc8aad00a53eff6ac500
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-09 15:16:24 -0600

    Add Caching to AboutUsSectionsDeleterService

--- AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsDeleterService.cs
+++ AsadaLisboaBackend.Services/AboutUsSections/AboutUsSectionsDeleterService.cs
@@ -1,6 +1,9 @@
-﻿using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
-using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
+﻿using AsadaLisboaBackend.RepositoryContracts.AboutUsSections;
+using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
+using AsadaLisboaBackend.ServiceContracts.MemoryCaches;
+using AsadaLisboaBackend.Services.MemoryCaches;
 using Microsoft.Extensions.Logging;
+using AsadaLisboaBackend.Utils;
 
 namespace AsadaLisboaBackend.Services.AboutUsSections
 {
@@ -8,11 +11,13 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
     {
         private readonly IAboutUsSectionsDeleterRepository _aboutUsSectionsDeleterRepository;
         private readonly ILogger<AboutUsSectionsDeleterService> _logger;
+        private readonly IMemoryCachesService _memoryCachesService;
 
-        public AboutUsSectionsDeleterService(IAboutUsSectionsDeleterRepository aboutUsSectionsDeleterRepository, ILogger<AboutUsSectionsDeleterService> logger)
+        public AboutUsSectionsDeleterService(IAboutUsSectionsDeleterRepository aboutUsSectionsDeleterRepository, ILogger<AboutUsSectionsDeleterService> logger, IMemoryCachesService memoryCachesService)
         {
             _aboutUsSectionsDeleterRepository = aboutUsSectionsDeleterRepository;
             _logger = logger;
+            _memoryCachesService = memoryCachesService;
         }
 
         public async Task DeleteAboutUsSection(Guid id)
@@ -20,6 +25,9 @@ namespace AsadaLisboaBackend.Services.AboutUsSections
             try
             {
                 await _aboutUsSectionsDeleterRepository.DeleteAboutUsSection(id);
+
+                _memoryCachesService.RemoveById(Constants.CACHE_ABOUT_US, id);
+                _memoryCachesService.ChangeVersion(Constants.CACHE_ABOUT_US);
 
                 _logger.LogInformation("Sobre nosotros con Id: {Id} eliminado exitosamente.", id);
             }
