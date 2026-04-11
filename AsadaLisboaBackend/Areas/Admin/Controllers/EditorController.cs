a5d3f292de8045e0199f97180c1dbbe0c0e4cc43
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-11 12:20:27 -0600

    Comments to admin EditorController.

--- AsadaLisboaBackend/Areas/Admin/Controllers/EditorController.cs
+++ AsadaLisboaBackend/Areas/Admin/Controllers/EditorController.cs
@@ -5,6 +5,9 @@ using AsadaLisboaBackend.ServiceContracts.Editors;
 
 namespace AsadaLisboaBackend.Areas.Admin.Controllers
 {
+    /// <summary>
+    /// Controller for enricher text editor, only accessible by admin users. 
+    /// </summary>
     [ApiController]
     [Area("Admin")]
     [ApiVersion("1.0")]
@@ -13,11 +16,20 @@ namespace AsadaLisboaBackend.Areas.Admin.Controllers
     {
         private readonly IEditorsAdderService _editorsAdderService;
 
+        /// <summary>
+        /// Constructor for EditorController.
+        /// </summary>
+        /// <param name="editorsAdderService">Service for adding editor content.</param>
         public EditorController(IEditorsAdderService editorsAdderService)
         {
             _editorsAdderService = editorsAdderService;
         }
 
+        /// <summary>
+        /// Create a temporal image for the enricher text editor. Uploads a image file to a temporal folder.
+        /// </summary>
+        /// <param name="editorRequestDTO">File object with the new temporal image.</param>
+        /// <returns>ActionResult containing the created temporal image url.</returns>
         [HttpPost("imagen-temp")]
         public async Task<IActionResult> CreateTemporalImage([FromForm] EditorRequestDTO editorRequestDTO)
         {
