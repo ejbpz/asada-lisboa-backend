0a6ad7f12591a6e438943845cc0e1dd4b710319d
Eduardo J. Brenes Perez <edubpz08@gmail.com>
2026-04-12 10:13:54 -0600

    Comments to public NosotrosController.

--- AsadaLisboaBackend/Areas/Cliente/Controllers/NosotrosController.cs
+++ AsadaLisboaBackend/Areas/Cliente/Controllers/NosotrosController.cs
@@ -5,6 +5,9 @@ using AsadaLisboaBackend.ServiceContracts.AboutUsSections;
 
 namespace AsadaLisboaBackend.Areas.Cliente.Controllers
 {
+    /// <summary>
+    /// Controller for getting about us news, public access.
+    /// </summary>
     [ApiController]
     [Area("Cliente")]
     [ApiVersion("1.0")]
@@ -13,11 +16,19 @@ namespace AsadaLisboaBackend.Areas.Cliente.Controllers
     {
         private readonly IAboutUsSectionsGetterService _aboutUsSectionsGetterService;
 
+        /// <summary>
+        /// Constructor for NosotrosController.
+        /// </summary>
+        /// <param name="aboutUsSectionsGetterService">Service for getting about us sections.</param>
         public NosotrosController(IAboutUsSectionsGetterService aboutUsSectionsGetterService)
         {
             _aboutUsSectionsGetterService = aboutUsSectionsGetterService;
         }
 
+        /// <summary>
+        /// Retrieves a list of 'About Us' sections ordered by their display order.
+        /// </summary>
+        /// <returns>An IActionResult containing a collection of 'About Us' section data.</returns>
         [HttpGet("")]
         public async Task<IActionResult> GetAboutUsSections()
         {
