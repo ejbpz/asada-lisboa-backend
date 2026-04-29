59b496847fda74973638dd8802870899a74f347d
Joseph Brenes Nieto <josephbrenes97@gmail.com>
2026-04-29 16:38:34 -0600

    Add SearchGlobalRequestDTO

---
+++ AsadaLisboaBackend.Models/DTOs/SearchGlobal/SearchGlobalRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace AsadaLisboaBackend.Models.DTOs.SearchGlobal
+{
+    public class SearchGlobalRequestDTO
+    {
+        public string Query { get; set; } = string.Empty;
+
+        public int Page { get; set; } = 1;
+
+        public int PageSize { get; set; } = 10;
+
+        public string? Type { get; set; }
+    }
+}
